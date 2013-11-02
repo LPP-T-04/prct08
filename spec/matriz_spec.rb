39ee7502cfca38238a99df9ffcc1dbaee435958f
EliezerCruzSuarez <alu0100611298@ull.edu.es>
2013-11-02 16:31:35 +0000

    Se incluye la espectativa para la suma de matrices.

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -3,6 +3,7 @@ require "matriz.rb"
 describe Matriz do
 	before :each do
 		@m1 = Matriz.new(2,2,[[1,2],[3,4]])
+		@m2 = Matriz.new(2,2,[[2,4],[6,8]])
 	end
 
 	describe "Se comprueba el numero de: " do
@@ -13,6 +14,7 @@ describe Matriz do
 			@m1.columnas.should eq(2)
 		end
 	end
+
 	describe "Acceder al subindice;" do
 		it "Para acceder al [0,0]" do
 			@m1.elemento[0][0].should eq(1)
@@ -27,9 +29,16 @@ describe Matriz do
 			@m1.elemento[1][1].should eq(4)
 		end
 	end
+
 	describe "Imprimir la matriz" do
 		it "del orden 2x2" do
 			@m1.to_s.should eq("1 2 \n3 4 \n")
+		end
+	end
+
+	describe "Suma de dos matrices" do
+		it "del orden mxn" do
+			@m1.+(m1).to_s.should eq(@m2)
 		end
 	end
 end
