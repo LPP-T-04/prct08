af41aaab9e5afb1c3323c47d1ca96cc575cfa71e
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-02 13:51:10 +0000

    Se incluye espectativa para almacenar y acceder al numero de filas y columnas.

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,5 +1,12 @@
 require "matriz.rb"
 
 describe Matriz do
-	
+	describe "Se comprueba el numero de: " do
+		it "Filas para una matriz 2x2" do
+			@filas.should eq(2)
+		end
+		it "Columnas de una matriz 2x2" do
+			@columnas.should eq(2)
+		end
+	end
 end
