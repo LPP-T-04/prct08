ab025b4436e7eba313d043e2e8678ed20fc8a473
alu0100596113 <alu0100596113@ull.edu.es>
2013-11-02 17:24:14 +0000

    Se incluye espectativa para multiplicacion de matrices.

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -4,6 +4,7 @@ describe Matriz do
 	before :each do
 		@m1 = Matriz.new(2,2,[[1,2],[3,4]])
 		@m2 = Matriz.new(2,2,[[2,4],[6,8]])
+		@m3 = Matriz.new(2,2,[[7,10],[15,22]])
 	end
 
 	describe "Se comprueba el numero de: " do
@@ -38,7 +39,7 @@ describe Matriz do
 
 	describe "Suma de dos matrices" do
 		it "del orden mxn" do
-			@m1.+(@m1).to_s.should eq(@m2.to_s)
+			@m1.+(@m1).to_s.should eq(@m3.to_s)
 		end
 	end
 
@@ -47,10 +48,16 @@ describe Matriz do
 			@m2.-(@m1).to_s.should eq(@m1.to_s)
 		end
 	end
-	
+
 	describe "Multiplicacion de un numero por una matriz" do
 		it "del orden mxn" do
 			@m1.*(2).to_s.should eq(@m2.to_s)
+		end
+	end
+
+	describe "Multiplicacion de matrices" do
+		it "del orden axn x nxb" do
+			@m1.x(@m1).to_s.should eq(@m3.to_s)
 		end
 	end
 end
